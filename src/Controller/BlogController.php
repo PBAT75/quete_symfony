87092f7581c7c6356a8b4337424a0b49268945b1
Patricia Batjom <patriciabatjom@gmail.com>
2018-11-03 17:03:27 +0100

    blog controller et slug

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patricia
+ * Date: 03/11/18
+ * Time: 15:30
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route ("blog/{slug}", requirements={"slug"="\b[a-z-]+\b"}, name="blog_show")
+     */
+    public function show($slug='article-sans-titre')
+    {
+            $space = ' ';
+            $slug = str_replace('-', $space, ucwords($slug, '-'));
+
+        return $this->render('blog/slug.html.twig', ['slug' => $slug]);
+    }
+}
